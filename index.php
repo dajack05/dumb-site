b283a77c1b347083ce26393320ec5d5c150e5870
Daniel Jackson <dajack05@gmail.com>
2025-03-28 00:28:37 -0400

    Added lots of npm and composer crap.

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+
+use Yosymfony\Toml\Toml;
+
+include "vendor/autoload.php";
+
+$config = Toml::ParseFile('site.toml');
+
+$page_name = substr($_SERVER['REQUEST_URI'], 1);
+if (strlen($page_name) <= 0) {
+    header("location:/" . $config['startpage']);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -6,11 +18,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <style>
+        <?php include "style.css"; ?>
+    </style>
+    <title><?= $config['title'] ?></title>
 </head>
 
 <body>
-
+    <?php include "template.php"; ?>
 </body>
 
 </html>
---
+++ pages/contact.md
@@ -0,0 +1,13 @@
+# Contact Us
+
+## Welcome To My World
+
+This is a test home page. I hope you like it.
+
+Oh, you don't? I don't care!
+
+## Another Area
+
+Oh wow, look at this area!
+
+It's so spacious and founcy!
